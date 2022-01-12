1356b955e2ba003caad87d37d90e9deb569bcfc8
Andagor <67076366+Andagor@users.noreply.github.com>
2022-01-12 21:41:13 +0200

    Update connect.php

--- App/DB/PDO/connect.php
+++ App/DB/PDO/connect.php
@@ -1,14 +1,10 @@
 <?php
-echo "in connect<br>";
 require_once '../app_config.php';
-echo "connect after require_once<br>";
 
 function GetConnectToDB()
 {
-    echo "in connect => GetConnectToDB<br>";
     try {
-    $conn = new PDO(DATABASE_TYPE.":host=".DATABASE_HOST."; port=".DATABASE_PORT."; dbname=".DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD);
-        echo "Подключение успешно установлено <br>";
+        $conn = new PDO(DATABASE_TYPE.":host=".DATABASE_HOST."; port=".DATABASE_PORT."; dbname=".DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD);
     }
     catch (PDOException $e) {
         echo "Ошибка: " . $e->getMessage();
